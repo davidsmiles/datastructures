7604eb2202542b6a94f4888799417f5cfa62c874
David Smiles <davidugbero@gmail.com>
2019-09-28 17:35:00 +0100

    implement 'add' method to binarytree

---
+++ binarytree.py
@@ -0,0 +1,31 @@
+"""
+implementing binary tree algorithm, adding new node
+"""
+from node import Node
+
+
+class BinaryTree:
+    def __init__(self, head: Node):
+        self.head = head
+
+    def add(self, new_node: Node):
+        current_node = self.head
+
+        while current_node:
+            if new_node.value == current_node.value:
+                raise ValueError('A node with this value exists.')
+            elif new_node.value < current_node.value:
+                if current_node.left:
+                    current_node = current_node.left
+                else:
+                    current_node.left = new_node
+                    break
+            elif new_node.value > current_node.value:
+                if current_node.right:
+                    current_node = current_node.right
+                else:
+                    current_node.right = new_node
+                    break
+
+
+
